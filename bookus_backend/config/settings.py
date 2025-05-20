a7cd9d5393b134f7dc98c27fc1e835e847d9c541
김종수 <rlwhdtn97@naver.com>
2025-05-20 11:37:38 +0900

    네이버 검색 api 구현완료

--- bookus_backend/config/settings.py
+++ bookus_backend/config/settings.py
@@ -86,9 +86,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-CROS_ALLOWED_ORIGINS=[
-    'http://127.0.0.1.5173',
-    'http://localhost:5173',
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
