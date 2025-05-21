553b747521a192e24ce52221d057b060dbd2c52b
김종수 <rlwhdtn97@naver.com>
2025-05-21 15:12:02 +0900

    메모 view 수정 및 url 추가

--- bookus_backend/config/settings.py
+++ bookus_backend/config/settings.py
@@ -55,6 +55,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser', 
+        'rest_framework.parsers.MultiPartParser', 
+
     ]
 }
 
@@ -121,6 +123,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Internationalization
